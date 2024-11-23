ce26875d5714f4076466e8d5931cae5a87afcaf2
tnnnnt <cyz2324141281@163.com>
2024-11-24 01:36:46 +0800

    now, we can random catch a video and upload to bilibili, good!!!

---
+++ GetVideo/get_video.py
@@ -0,0 +1,38 @@
+import requests
+import sys
+
+
+def download_video(u, path):
+	res = requests.get(u, stream=True)
+	if res.status_code == 200:
+		with open(path, 'wb') as file:
+			for chunk in res.iter_content(chunk_size=1024):
+				if chunk:
+					file.write(chunk)
+		return True
+	else:
+		return False
+
+
+def get_video_0():
+	url = 'https://api.kuleu.com/api/xjj?type=json'
+	response = requests.get(url)
+	js = response.json()
+	if js['code'] == 200:
+		return download_video(js['video'], video_path)
+
+
+def get_video_1():
+	url = 'https://api.kuleu.com/api/MP4_xiaojiejie?type=json'
+	response = requests.get(url)
+	js = response.json()
+	if js['code'] == 200:
+		return download_video(js['mp4_video'], video_path)
+
+
+if __name__ == "__main__":
+	args = sys.argv
+	video_path = args[1]
+	while True:
+		if get_video_0() or get_video_1():
+			break
--- MediaMule/main.cpp
+++ MediaMule/main.cpp
@@ -5,20 +5,28 @@
 #include <iostream>
 #include <windows.h>
 int main(int argc, char* argv[]) {
+	const std::string version = "2024.11.24";
 	const std::string program_path = std::filesystem::absolute(argv[0]).parent_path().string();
-	const std::string biliup_exe_path = "\"" + program_path + "/biliup.exe\"";
+	const std::string biliup_exe_path = program_path + "/biliup.exe";
+	const std::string get_video_python_script_path = "\"" + program_path + "/get_video.py\"";
 	const std::string cookie_json_path = "\"" + program_path + "/cookie.json\"";
 	const std::string config_json_path = program_path + "/config.json";
 	std::ifstream config_json_file(config_json_path);
 	const nlohmann::json config_json_data = nlohmann::json::parse(config_json_file);
-	const std::string version = config_json_data["version"];
 	const bool copyright = config_json_data["copyright"];
 	const std::string title = config_json_data["title"];
-	const std::string tmp_dir = config_json_data["tmp_dir"];
+	const std::string tags = config_json_data["tags"];
+	const std::string video_path = config_json_data["video_path"];
 	const int time_mode = config_json_data["time_mode"];
 	const int time_interval = config_json_data["time_interval"];
 	const std::vector<std::string> timelist = config_json_data["timelist"];
-	//std::string cmd = "ls";
-	//int result = system("ls");
+	std::string cmd = "python " + get_video_python_script_path + " \"" + video_path + "\"";
+	system(cmd.c_str());
+	cmd = biliup_exe_path + " --user-cookie " + cookie_json_path + " upload --copyright 1 --tag \"" + tags + "\" ";
+	if(title != ""){
+        cmd += "--title \"" + title + "\" ";
+	}
+	cmd += "\"" + video_path + "\"";
+	system(cmd.c_str());
 	return 0;
 }
