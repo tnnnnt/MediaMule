30c19ce70e37e35750463001165b4ad2385df3c2
tnnnnt <cyz2324141281@163.com>
2024-11-24 12:19:34 +0800

    use timestamp as the video title

--- MediaMule/main.cpp
+++ MediaMule/main.cpp
@@ -4,6 +4,13 @@
 #include <fstream>
 #include <iostream>
 #include <windows.h>
+#include <chrono>
+static std::string get_timestamp() {
+	auto now = std::chrono::system_clock::now();
+	auto duration = now.time_since_epoch();
+	auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
+	return std::to_string(timestamp);
+}
 int main(int argc, char* argv[]) {
 	const std::string version = "2024.11.24";
 	const std::string program_path = std::filesystem::absolute(argv[0]).parent_path().string();
@@ -14,7 +21,6 @@ int main(int argc, char* argv[]) {
 	std::ifstream config_json_file(config_json_path);
 	const nlohmann::json config_json_data = nlohmann::json::parse(config_json_file);
 	const bool copyright = config_json_data["copyright"];
-	const std::string title = config_json_data["title"];
 	const std::string tags = config_json_data["tags"];
 	const std::string video_path = config_json_data["video_path"];
 	const int time_mode = config_json_data["time_mode"];
@@ -22,11 +28,7 @@ int main(int argc, char* argv[]) {
 	const std::vector<std::string> timelist = config_json_data["timelist"];
 	std::string cmd = "python " + get_video_python_script_path + " \"" + video_path + "\"";
 	system(cmd.c_str());
-	cmd = biliup_exe_path + " --user-cookie " + cookie_json_path + " upload --copyright 1 --tag \"" + tags + "\" ";
-	if(title != ""){
-        cmd += "--title \"" + title + "\" ";
-	}
-	cmd += "\"" + video_path + "\"";
+	cmd = biliup_exe_path + " --user-cookie " + cookie_json_path + " upload --copyright 1 --tag \"" + tags + "\" --title " + get_timestamp() + " \"" + video_path + "\"";
 	system(cmd.c_str());
 	return 0;
 }
